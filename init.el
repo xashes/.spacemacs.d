8bc9bf4208f7d7fda309b36f3bba7682a0c65c29
Keller <kellerx0319@gmail.com>
2018-11-07 22:16:18 +0800

    bind "C-;" to 'complete-symbol in global map

--- init.el
+++ init.el
@@ -543,6 +543,7 @@ you should place your code here."
     "od" 'find-by-pinyin-dired
     )
   (define-key evil-insert-state-map (kbd "C-SPC") 'toggle-input-method)
+  (define-key global-map (kbd "C-;") 'complete-symbol)
   (add-hook 'anaconda-mode-hook
             (lambda ()
               (define-key anaconda-mode-map (kbd "C-;") 'anaconda-mode-complete))
